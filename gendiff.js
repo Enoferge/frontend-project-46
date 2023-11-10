9578c141873c22404e3ed82a3c14b76f18a3a6c3
Enoferge <jade.escapist@yandex.ru>
2023-11-10 21:06:33 +0300

    [2]: add arguments, format option, change help description

--- gendiff.js
+++ gendiff.js
@@ -5,7 +5,10 @@ const program = new Command();
 program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .version('0.1.0');
+    .version('0.1.0')
+    .helpOption('-h, --help', 'output usage information')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format <type>', 'output format');
 
 program.parse()
 
