b1d651247c7326c3aea84ff3937b337a35041fe6
lanarodionova <jade.escapist@yandex.ru>
2023-11-13 19:18:25 +0300

    [4]: add get-diff function, add ansciinema for demonstration

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Enoferge/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Enoferge/frontend-project-46/actions)
+
+* Asciinema for main function demonstration:
+https://asciinema.org/a/gMlGLdAiIsfvpij1QkHIrPwRM
--- gendiff.js
+++ gendiff.js
@@ -4,6 +4,39 @@ import parser from './parser.js';
 
 const program = new Command();
 
+const getDiff = (data1, data2) => {
+    const data2Copy = {...data2}
+    let list = []
+
+    Object.entries(data1).forEach(([key, value]) => {
+        if (data2Copy[key]) {
+            if (data2Copy[key] === value) {
+                list.push({key, value, sign: undefined})
+            } else {
+                list.push({key, value, sign: true})
+                list.push({key, value: data2Copy[key], sign: false})
+            }
+            delete data2Copy[key]
+        } else {
+            list.push({key, value, sign: true})
+        }
+    })
+
+    Object.entries(data2Copy).forEach(([key, value]) => {
+        list.push({key, value, sign: false})
+    })
+
+    list.sort((a,b) => a.key > b.key ? 1 : -1)
+
+    const listToString = list.reduce((acc, {key, value, sign}) => {
+        const signSymbol = sign === true ? '+' : sign === false ? '-' : ' '
+        acc += ` ${signSymbol} ${key}: ${value}\n`
+        return acc
+    }, '')
+
+    return `{\n${listToString}}`
+}
+
 program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
@@ -14,6 +47,9 @@ program
     .action((filepath1, filepath2) => {
         const data1 = parser(filepath1);
         const data2 = parser(filepath2);
+        
+        const result = getDiff(data1, data2)
+        console.log(result)
     })
 
 program.parse()
