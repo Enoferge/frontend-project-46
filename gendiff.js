5d0b81331347810b6c520def7c8d4df9bd1d17a3
Enoferge <jade.escapist@yandex.ru>
2023-11-14 02:18:57 +0300

    [5]: add eslint, fix files, add gitignore

--- gendiff.js
+++ gendiff.js
@@ -5,53 +5,58 @@ import parser from './parser.js';
 const program = new Command();
 
 const getDiff = (data1, data2) => {
-    const data2Copy = {...data2}
-    let list = []
+  const data2Copy = { ...data2 };
+  const list = [];
 
-    Object.entries(data1).forEach(([key, value]) => {
-        if (data2Copy[key]) {
-            if (data2Copy[key] === value) {
-                list.push({key, value, sign: undefined})
-            } else {
-                list.push({key, value, sign: true})
-                list.push({key, value: data2Copy[key], sign: false})
-            }
-            delete data2Copy[key]
-        } else {
-            list.push({key, value, sign: true})
-        }
-    })
+  Object.entries(data1).forEach(([key, value]) => {
+    if (data2Copy[key]) {
+      if (data2Copy[key] === value) {
+        list.push({ key, value, sign: undefined });
+      } else {
+        list.push({ key, value, sign: true });
+        list.push({ key, value: data2Copy[key], sign: false });
+      }
+      delete data2Copy[key];
+    } else {
+      list.push({ key, value, sign: true });
+    }
+  });
 
-    Object.entries(data2Copy).forEach(([key, value]) => {
-        list.push({key, value, sign: false})
-    })
+  Object.entries(data2Copy).forEach(([key, value]) => {
+    list.push({ key, value, sign: false });
+  });
 
-    list.sort((a,b) => a.key > b.key ? 1 : -1)
+  list.sort((a, b) => a.key > b.key ? 1 : -1);
 
-    const listToString = list.reduce((acc, {key, value, sign}) => {
-        const signSymbol = sign === true ? '+' : sign === false ? '-' : ' '
-        acc += ` ${signSymbol} ${key}: ${value}\n`
-        return acc
-    }, '')
+  const listToString = list.reduce((acc, { key, value, sign }) => {
+    let signSymbol = '+';
+    if (!sign) {
+      signSymbol = sign === false ? '-' : ' ';
+    }
 
-    return `{\n${listToString}}`
-}
+    // eslint-disable-next-line no-param-reassign
+    acc += ` ${signSymbol} ${key}: ${value}\n`;
+    return acc;
+  }, '');
+
+  return `{\n${listToString}}`;
+};
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.1.0')
-    .helpOption('-h, --help', 'output usage information')
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --format <type>', 'output format')
-    .action((filepath1, filepath2) => {
-        const data1 = parser(filepath1);
-        const data2 = parser(filepath2);
-        
-        const result = getDiff(data1, data2)
-        console.log(result)
-    })
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1.0')
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    const data1 = parser(filepath1);
+    const data2 = parser(filepath2);
 
-program.parse()
+    const result = getDiff(data1, data2);
+    console.log(result);
+  });
 
-export default program
+program.parse();
+
+export default program;
--- parser.js
+++ parser.js
@@ -1,15 +1,15 @@
-import { readFileSync } from 'fs'; 
+import { readFileSync } from 'fs';
 import path from 'path';
-import { cwd } from 'process'
+import { cwd } from 'process';
 
 export default (filePath) => {
-    const currentDir = cwd()
-    const extension = filePath.split('.').pop();
-    const preparedFilePath = path.resolve(currentDir, filePath)
-    const data = readFileSync(preparedFilePath)
+  const currentDir = cwd();
+  const extension = filePath.split('.').pop();
+  const preparedFilePath = path.resolve(currentDir, filePath);
+  const data = readFileSync(preparedFilePath);
 
-    if (extension === 'json') {
-        return JSON.parse(data)
-    }
-    return data
-} 
+  if (extension === 'json') {
+    return JSON.parse(data);
+  }
+  return data;
+};
