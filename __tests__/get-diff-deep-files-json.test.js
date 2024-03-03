f717b872da90e5863e203823317edcccac3177f0
lanarodionova <jade.escapist@yandex.ru>
2024-03-03 20:24:48 +0300

    add json test

--- __tests__/get-diff-deep-files-json.test.js
+++ __tests__/get-diff-deep-files-json.test.js
@@ -1,12 +1,12 @@
 import getAbsFixturePath from '../helpers/get-abs-fixture-path.js';
 import { FORMATTERS } from '../src/constants.js';
 import getDiff from '../src/index.js';
-// import parser from '../src/parser.js';
+import parser from '../src/parser.js';
 
 test('deep JSON files diff with format json', () => {
-  // const expectedJson = parser(getAbsFixturePath('result-expected.json'));
+  const expectedJson = parser(getAbsFixturePath('result-expected.json'));
   const result = getDiff(getAbsFixturePath('file-deep1.json'), getAbsFixturePath('file-deep2.json'), FORMATTERS.JSON);
 
   expect(() => JSON.parse(result)).not.toThrow();
-  // expect(result).toEqual(expectedJson);
+  expect(result).toEqual(JSON.stringify(expectedJson));
 });
